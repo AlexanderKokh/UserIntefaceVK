2791e5d5749aa6f6e1ea73cb22e144664cc40fc2
IOS <alekokh@yandex.ru>
2020-07-22 15:27:24 +0400

    Кастомные  UI-компоненты
    Добавлен View из двух subview (тень и скргуленные края для аватарки друга)
    Добавлен контрол со счетчиком лайков для экрана CollectionView, отображающим аватар друга.

--- CloneVK/UserCell.swift
+++ CloneVK/UserCell.swift
@@ -12,4 +12,24 @@ class UserCell: UITableViewCell {
     
     @IBOutlet weak var friendNameLabel: UILabel!
     @IBOutlet weak var friendImage: UIImageView!
+    @IBOutlet weak var friendContainerView: UIView!
+      
+      override func prepareForReuse() {
+          super.prepareForReuse()
+      }
+      
+      override func awakeFromNib() {
+          super.awakeFromNib()
+        
+          friendContainerView.layer.cornerRadius = friendContainerView.bounds.width/2
+          friendContainerView.layer.shadowColor = UIColor.black.cgColor
+          friendContainerView.layer.shadowRadius = 10
+          friendContainerView.layer.shadowOpacity = 0.5
+          friendContainerView.layer.shadowOffset = .zero
+          friendContainerView.layer.shadowPath = UIBezierPath(ovalIn: friendContainerView.bounds).cgPath
+          
+          friendImage.layer.cornerRadius = friendContainerView.bounds.width/2
+          friendImage.clipsToBounds = true
+        
+      }
 }
